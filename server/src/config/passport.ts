e7c5c05748082fb7e7542c4bd0dc86ca4107736e
FANIC <sunnyhuang0108@gmail.com>
2025-07-20 04:44:42 +0800

    0444

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -1,8 +1,6 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import { Strategy as FacebookStrategy } from 'passport-facebook';
-// @ts-ignore
-import { Strategy as LineStrategy } from 'passport-line';
 import { UserModel } from '../models/User';
 
 // Google OAuth 策略
@@ -12,11 +10,8 @@ passport.use(new GoogleStrategy({
   callbackURL: 'http://localhost:5000/api/auth/google/callback'
 }, async (accessToken: string, refreshToken: string, profile: any, done: any) => {
   try {
-    // 查找或創建用戶
     let user = await UserModel.findByProviderId(profile.id, 'google');
-    
     if (!user) {
-      // 創建新用戶
       const userData = {
         name: profile.displayName || 'Unknown User',
         email: profile.emails?.[0]?.value || '',
@@ -24,10 +19,8 @@ passport.use(new GoogleStrategy({
         provider_id: profile.id,
         avatar_url: profile.photos?.[0]?.value
       };
-      
       user = await UserModel.create(userData);
     }
-    
     return done(null, user);
   } catch (error) {
     return done(error as Error);
@@ -42,11 +35,8 @@ passport.use(new FacebookStrategy({
   profileFields: ['id', 'displayName', 'emails', 'photos']
 }, async (accessToken: string, refreshToken: string, profile: any, done: any) => {
   try {
-    // 查找或創建用戶
     let user = await UserModel.findByProviderId(profile.id, 'facebook');
-    
     if (!user) {
-      // 創建新用戶
       const userData = {
         name: profile.displayName || 'Unknown User',
         email: profile.emails?.[0]?.value || '',
@@ -54,52 +44,18 @@ passport.use(new FacebookStrategy({
         provider_id: profile.id,
         avatar_url: profile.photos?.[0]?.value
       };
-      
       user = await UserModel.create(userData);
     }
-    
     return done(null, user);
   } catch (error) {
     return done(error as Error);
   }
 }));
 
-// LINE OAuth 策略
-passport.use(new LineStrategy({
-  channelID: process.env.LINE_CHANNEL_ID || '',
-  channelSecret: process.env.LINE_CHANNEL_SECRET || '',
-  callbackURL: 'http://localhost:5000/api/auth/line/callback',
-  scope: ['profile', 'openid', 'email']
-}, async (accessToken: string, refreshToken: string, profile: any, done: any) => {
-  try {
-    // 查找或創建用戶
-    let user = await UserModel.findByProviderId(profile.id, 'line');
-    
-    if (!user) {
-      // 創建新用戶
-      const userData = {
-        name: profile.displayName || 'Unknown User',
-        email: profile.emails?.[0]?.value || '',
-        provider: 'line' as const,
-        provider_id: profile.id,
-        avatar_url: profile.pictureUrl
-      };
-      
-      user = await UserModel.create(userData);
-    }
-    
-    return done(null, user);
-  } catch (error) {
-    return done(error as Error);
-  }
-}));
-
-// 序列化用戶
 passport.serializeUser((user: any, done: any) => {
   done(null, user.id);
 });
 
-// 反序列化用戶
 passport.deserializeUser(async (id: number, done: any) => {
   try {
     const user = await UserModel.findById(id);
